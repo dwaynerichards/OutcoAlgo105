86bf6e85adafa09bb54bd819efbaa9b1179451c3
dwayneRichards <dwaynerichards@gmail.com>
2022-09-22 11:48:42 -0400

    feat: freqCount TP dfs implementinged with doublyLL

--- target_practice_prompts/js/w2_d4_binary_tree_traversal.js
+++ target_practice_prompts/js/w2_d4_binary_tree_traversal.js
@@ -4,36 +4,147 @@
 
 'use strict';
 
+const { timeStamp } = require('console');
+
 // DO NOT EDIT
 // Node class for a binary tree node
 class TreeNode {
-  constructor(value){
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+class ListNode {
+    previous = null;
+    next = null;
+    constructor(value) {
+        this.value = value;
+    }
+}
+class LinkList {
+    head = null;
+    tail = null;
+    size = 0;
+    storage;
+    constructor() {
+        this.storage = new Map();
+    }
+    append(value) {
+        const node = new ListNode(value);
+        if (this.head === null) {
+            this.head = node;
+        } else {
+            node.previous = this.tail;
+            this.tail.next = node;
+            this.updateCache([this.node.previous]);
+        }
+        this.tail = node;
+        this.size++;
+        this.updateCache([this.tail]);
+    }
+    delete(i) {
+        //head = 0 index
+        if (i > this.size) return null; //iterate until index is i- current will be index to delete
+        if (i === this.size - 1) {
+            this.storage.delete(this.tail.value);
+            this.tail = this.tail.previous;
+            this.tail.next = null;
+            this.size--;
+            return;
+        }
+        let [current, index] = [this.head, 0];
+        while (index < i) {
+            current = current.next;
+            index;
+        }
+        current.prev.next = current.next;
+        this.size--;
+        this.storage.delete(current);
+        this.updateCache([current.prev, current.next]);
+    }
+    updateCache(nodesArr) {
+        nodesArr.forEach((node) => {
+            const { previous, next } = node;
+            this.storage.set(node.value, {
+                previous,
+                next,
+            });
+        });
+    }
+}
+class Queue extends LinkList {
+    dequeue() {
+        const oldHead = this.head;
+        this.head = oldHead.next; //head mutated to head.next
+        this.head.previous = null;
+        this.storage.delete(oldHead.value);
+        //delete head from storage
+        this.updateCache([this.head]);
+        this.size--;
+        return oldHead;
+    }
 }
 
 // DO NOT EDIT
 // generate tree from array
+/**
+ *              4
+ *            /   \
+ *          2       5
+ *        /   \       \
+ *      1       3       7
+ *                    /   \
+ *                  6      8
+const arr = [4, 2, 5, 1, 3, null, 7, null, null, null, null, 6, 8];
+ * 
+ */
 function deserialize(arr) {
-  if (arr.length === 0) { return null; }
-  let root = new TreeNode(arr[0]);
-  let queue = [root];
-  for(let i = 1; i < arr.length; i += 2) {
-    let current = queue.shift();
-    if (arr[i] !== null) {
-      current.left = new TreeNode(arr[i]);
-      queue.push(current.left);
+    if (arr.length === 0) {
+        return null;
     }
-    if (arr[i + 1] !== null && arr[i + 1] !== undefined) {
-      current.right = new TreeNode(arr[i + 1]);
-      queue.push(current.right);
+    let root = new TreeNode(arr[0]);
+    let queue = [root];
+    for (let i = 1; i < arr.length; i += 2) {
+        //step through arr starting at index 1
+        let current = queue.shift(); //instatiate current so that you might be able to link othr nodes
+        if (arr[i] !== null) {
+            //if not null, next item in arra should be placed at current's left (BFS)
+            current.left = new TreeNode(arr[i]);
+            queue.push(current.left); //then pushed into queue to continue interation
+        }
+        if (arr[i + 1] !== null && arr[i + 1] !== undefined) {
+            //BFS takes the next 2 items in the arr as the parent's children
+            current.right = new TreeNode(arr[i + 1]); //
+            queue.push(current.right);
+        }
     }
-  }
-  return root;
+    return root;
 }
 
+function deserializeLL(arr) {
+    const root = new TreeNode(arr[0]);
+    const queue = new Queue();
+    queue.append(root.val);
+    let i = 1;
+    while (queue.size > 0) {
+        //instantiate current by dequeing queue
+        //iterate through arr srating at index 1- incrementing up 2
+        const current = queue.dequeue();
+        if (arr[i]) {
+            queue.append(arr[i]);
+            current.left = new TreeNode(arr[i]);
+        }
+        if (arr[i + 1]) {
+            queue.append(arr[i + 1]);
+            current.right = new TreeNode(arr[i + 1]);
+        }
+        i += 2;
+    }
+    return root;
+}
+
+//linkList as queue, capture head, make head into head.next,
 
 /**
  *
@@ -62,11 +173,10 @@ function deserialize(arr) {
  *                  6      8
  */
 
- // DO NOT EDIT
+// DO NOT EDIT
 const arr = [4, 2, 5, 1, 3, null, 7, null, null, null, null, 6, 8];
 
 const sampleTree = deserialize(arr);
-
 
 /**
  *  2. Given the example output binary search tree from Problem 1, what would
@@ -77,7 +187,6 @@ const sampleTree = deserialize(arr);
  *     c. IN-ORDER DEPTH first traversal:
  *     d. POST-ORDER DEPTH first traversal:
  */
-
 
 /**
  *  3a. Using a queue and while loop write a function that takes the root of a
@@ -92,9 +201,8 @@ const sampleTree = deserialize(arr);
  */
 
 function bfs(node) {
-  // YOUR WORK HERE
+    // YOUR WORK HERE
 }
-
 
 /*
  *  3b. Using recursion, write a function that takes in a tree node and outputs
@@ -106,9 +214,8 @@ function bfs(node) {
  *      NOTE: Confirm with your answer from problem 2b.
  */
 function dfsPre(node) {
-  // YOUR WORK HERE
+    // YOUR WORK HERE
 }
-
 
 /**
  *  3c. Using recursion, write a function that takes in a tree node and outputs
@@ -120,9 +227,8 @@ function dfsPre(node) {
  *      NOTE: Confirm with your answer from problem 2b.
  */
 function dfsIn(node) {
-  // YOUR WORK HERE
+    // YOUR WORK HERE
 }
-
 
 /**
  *  3d. Using recursion, write a function that takes in a tree node and outputs
@@ -133,148 +239,174 @@ function dfsIn(node) {
  *
  *      NOTE: Confirm with your answer from problem 2d.
  */
- function dfsPost(node) {
-   // YOUR WORK HERE
- }
+function dfsPost(node) {
+    // YOUR WORK HERE
+}
 
+////////////////////////////////////////////////////////////
+///////////////  DO NOT TOUCH TEST BELOW!!!  ///////////////
+////////////////////////////////////////////////////////////
 
- ////////////////////////////////////////////////////////////
- ///////////////  DO NOT TOUCH TEST BELOW!!!  ///////////////
- ////////////////////////////////////////////////////////////
+// custom assert function to handle tests
+// input: count {Array} - keeps track out how many tests pass and how many total
+//        in the form of a two item array i.e., [0, 0]
+// input: name {String} - describes the test
+// input: test {Function} - performs a set of operations and returns a boolean
+//        indicating if test passed
+// output: {undefined}
+function assert(count, name, test) {
+    if (!count || !Array.isArray(count) || count.length !== 2) {
+        count = [0, '*'];
+    } else {
+        count[1]++;
+    }
 
+    let pass = 'false';
+    let errMsg = null;
+    try {
+        if (test()) {
+            pass = ' true';
+            count[0]++;
+        }
+    } catch (e) {
+        errMsg = e;
+    }
+    console.log('  ' + (count[1] + ')   ').slice(0, 5) + pass + ' : ' + name);
+    if (errMsg !== null) {
+        console.log('       ' + errMsg + '\n');
+    }
+}
 
- // custom assert function to handle tests
- // input: count {Array} - keeps track out how many tests pass and how many total
- //        in the form of a two item array i.e., [0, 0]
- // input: name {String} - describes the test
- // input: test {Function} - performs a set of operations and returns a boolean
- //        indicating if test passed
- // output: {undefined}
- function assert(count, name, test) {
-   if(!count || !Array.isArray(count) || count.length !== 2) {
-     count = [0, '*'];
-   } else {
-     count[1]++;
-   }
+// compare if two flat arrays are equal
+function arraysEqual(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
 
-   let pass = 'false';
-   let errMsg = null;
-   try {
-     if (test()) {
-       pass = ' true';
-       count[0]++;
-     }
-   } catch(e) {
-     errMsg = e;
-   }
-   console.log('  ' + (count[1] + ')   ').slice(0,5) + pass + ' : ' + name);
-   if (errMsg !== null) {
-     console.log('       ' + errMsg + '\n');
-   }
- }
+// generate test tree for the rest of the tests
+const test = new TreeNode(4);
+test.left = new TreeNode(2);
+test.left.left = new TreeNode(1);
+test.left.right = new TreeNode(3);
+test.right = new TreeNode(5);
+test.right.right = new TreeNode(7);
+test.right.right.left = new TreeNode(6);
+test.right.right.right = new TreeNode(8);
 
+console.log('Problem 1 tests');
+let testCount = [0, 0];
 
- // compare if two flat arrays are equal
- function arraysEqual(arr1, arr2) {
-   if (arr1.length !== arr2.length) { return false; }
-   for (let i = 0; i < arr1.length; i++) {
-     if (arr1[i] !== arr2[i]) { return false; }
-   }
-   return true;
- }
+assert(testCount, 'able to build tree as indicated in diagram', () => {
+    return (
+        sampleTree !== null &&
+        sampleTree.value === 4 &&
+        sampleTree.left.value === 2 &&
+        sampleTree.left.left.value === 1 &&
+        sampleTree.left.right.value === 3 &&
+        sampleTree.right.value === 5 &&
+        sampleTree.right.right.value === 7 &&
+        sampleTree.right.right.left.value === 6 &&
+        sampleTree.right.right.right.value === 8
+    );
+});
 
+console.log('PASSED: ' + testCount[0] + ' / ' + testCount[1], '\n\n');
 
- // generate test tree for the rest of the tests
- const test = new TreeNode(4);
- test.left = new TreeNode(2);
- test.left.left = new TreeNode(1);
- test.left.right = new TreeNode(3);
- test.right = new TreeNode(5);
- test.right.right = new TreeNode(7);
- test.right.right.left = new TreeNode(6);
- test.right.right.right = new TreeNode(8);
+console.log('breadth first search tests');
+testCount = [0, 0];
 
+assert(
+    testCount,
+    'able to return values in breadth first order - ' +
+        '[4, 2, 5, 1, 3, 7, 6, 8]',
+    () => {
+        let results = bfs(test);
+        return (
+            results !== undefined &&
+            arraysEqual(results, [4, 2, 5, 1, 3, 7, 6, 8])
+        );
+    }
+);
 
- console.log('Problem 1 tests');
- let testCount = [0, 0];
+assert(testCount, 'returns an empty array for an empty tree', () => {
+    let results = bfs(deserialize([]));
+    return results !== undefined && arraysEqual(results, []);
+});
 
- assert(testCount, 'able to build tree as indicated in diagram', () => {
-   return sampleTree !== null &&
-     sampleTree.value === 4 &&
-     sampleTree.left.value === 2 &&
-     sampleTree.left.left.value === 1 &&
-     sampleTree.left.right.value === 3 &&
-     sampleTree.right.value === 5 &&
-     sampleTree.right.right.value === 7 &&
-     sampleTree.right.right.left.value === 6 &&
-     sampleTree.right.right.right.value === 8;
- });
+console.log('PASSED: ' + testCount[0] + ' / ' + testCount[1], '\n\n');
 
- console.log('PASSED: ' + testCount[0] + ' / ' + testCount[1], '\n\n');
+console.log('pre-order depth first search tests');
+testCount = [0, 0];
 
+assert(
+    testCount,
+    'able to return values pre-order depth first order - ' +
+        '[4, 2, 1, 3, 5, 7, 6, 8]',
+    () => {
+        let results = dfsPre(test);
+        return (
+            results !== undefined &&
+            arraysEqual(results, [4, 2, 1, 3, 5, 7, 6, 8])
+        );
+    }
+);
 
- console.log('breadth first search tests');
- testCount = [0, 0];
+assert(testCount, 'returns an empty array for an empty tree', () => {
+    let results = dfsPre(deserialize([]));
+    return results !== undefined && arraysEqual(results, []);
+});
 
- assert(testCount, 'able to return values in breadth first order - ' +
-   '[4, 2, 5, 1, 3, 7, 6, 8]', () => {
-   let results = bfs(test);
-   return results !== undefined && arraysEqual(results, [4, 2, 5, 1, 3, 7, 6, 8]);
- });
+console.log('PASSED: ' + testCount[0] + ' / ' + testCount[1], '\n\n');
 
- assert(testCount, 'returns an empty array for an empty tree', () => {
-   let results = bfs(deserialize([]));
-   return results !== undefined && arraysEqual(results, []);
- });
+console.log('in-order depth first search tests');
+testCount = [0, 0];
 
- console.log('PASSED: ' + testCount[0] + ' / ' + testCount[1], '\n\n');
+assert(
+    testCount,
+    'able to return values pin-order depth first order - ' +
+        '[1, 2, 3, 4, 5, 6, 7, 8]',
+    () => {
+        let results = dfsIn(test);
+        return (
+            results !== undefined &&
+            arraysEqual(results, [1, 2, 3, 4, 5, 6, 7, 8])
+        );
+    }
+);
 
+assert(testCount, 'returns an empty array for an empty tree', () => {
+    let results = dfsIn(deserialize([]));
+    return results !== undefined && arraysEqual(results, []);
+});
 
- console.log('pre-order depth first search tests');
- testCount = [0, 0];
+console.log('PASSED: ' + testCount[0] + ' / ' + testCount[1], '\n\n');
 
- assert(testCount, 'able to return values pre-order depth first order - ' +
-   '[4, 2, 1, 3, 5, 7, 6, 8]', () => {
-   let results = dfsPre(test);
-   return results !== undefined && arraysEqual(results, [4, 2, 1, 3, 5, 7, 6, 8]);
- });
+console.log('post-order depth first search tests');
+testCount = [0, 0];
 
- assert(testCount, 'returns an empty array for an empty tree', () => {
-   let results = dfsPre(deserialize([]));
-   return results !== undefined && arraysEqual(results, []);
- });
+assert(
+    testCount,
+    'able to return values post-order depth first order - ' +
+        '[1, 3, 2, 6, 8, 7, 5, 4]',
+    () => {
+        let results = dfsPost(test);
+        return (
+            results !== undefined &&
+            arraysEqual(results, [1, 3, 2, 6, 8, 7, 5, 4])
+        );
+    }
+);
 
- console.log('PASSED: ' + testCount[0] + ' / ' + testCount[1], '\n\n');
+assert(testCount, 'returns an empty array for an empty tree', () => {
+    let results = dfsPost(deserialize([]));
+    return results !== undefined && arraysEqual(results, []);
+});
 
-
- console.log('in-order depth first search tests');
- testCount = [0, 0];
-
- assert(testCount, 'able to return values pin-order depth first order - ' +
-   '[1, 2, 3, 4, 5, 6, 7, 8]', () => {
-   let results = dfsIn(test);
-   return results !== undefined && arraysEqual(results, [1, 2, 3, 4, 5, 6, 7, 8]);
- });
-
- assert(testCount, 'returns an empty array for an empty tree', () => {
-   let results = dfsIn(deserialize([]));
-   return results !== undefined && arraysEqual(results, []);
- });
-
- console.log('PASSED: ' + testCount[0] + ' / ' + testCount[1], '\n\n');
-
- console.log('post-order depth first search tests');
- testCount = [0, 0];
-
- assert(testCount, 'able to return values post-order depth first order - ' +
-   '[1, 3, 2, 6, 8, 7, 5, 4]', () => {
-   let results = dfsPost(test);
-   return results !== undefined && arraysEqual(results, [1, 3, 2, 6, 8, 7, 5, 4]);
- });
-
- assert(testCount, 'returns an empty array for an empty tree', () => {
-   let results = dfsPost(deserialize([]));
-   return results !== undefined && arraysEqual(results, []);
- });
-
- console.log('PASSED: ' + testCount[0] + ' / ' + testCount[1], '\n\n');
+console.log('PASSED: ' + testCount[0] + ' / ' + testCount[1], '\n\n');
